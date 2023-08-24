75b9e19b7aecc8bae1049b4b16250d2d6c60ce7e
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-08-24 19:35:04 +0530

    saving function practise

---
+++ Python Programming Fundamentals/Function/function.py
@@ -0,0 +1,46 @@
+list_1 = [6, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# len
+print(len(list_1))
+
+# max
+print(max(list_1))
+
+# min
+print(min(list_1))
+
+# sum
+print(sum(list_1))
+
+# sort
+list_1.sort()
+print(list_1)
+
+# reverse
+list_1.reverse()
+print(list_1)
+
+# append
+list_1.append(10)
+print(list_1)
+
+# pop
+list_1.pop()
+print(list_1)
+
+# remove
+# list_1.remove(10)
+# print(list_1)
+
+# insert
+# list_1.insert(0, 10)
+# print(list_1)
+
+# index
+# print(list_1.index(10))
+
+# count
+# print(list_1.count(10))
+
+# sum
+print(sum(list_1))
---
+++ Python Programming Fundamentals/Function/user_function.py
@@ -0,0 +1,15 @@
+def add_1(a):
+    return a + 1
+
+
+add_1(1)
+print(add_1(1))
+
+
+def mul(a, b):
+    return a * b
+
+
+print(mul(2, 3))
+
+print(mul("hii ", 2))
