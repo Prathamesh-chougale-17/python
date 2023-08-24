e573ba85ce4f2f2982fa86018a3a6dcd8965998f
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-08-24 17:58:39 +0530

    saving condition and branching practise

---
+++ Python Programming Fundamentals/Conditions and Branching/branching.py
@@ -0,0 +1,18 @@
+# branching
+age = 18
+if age > 18:  # semocolon needed
+    print("You can enter")
+elif age == 18:
+    print("go see Pink Floyd")
+else:
+    print("move on, you are too young")
+print("nice day")
+
+# logical operators
+
+# and
+album_year = 1980
+if album_year > 1979 and album_year < 1990:
+    print("Album year was in between 1980 and 1989")
+else:
+    print("Album year was not in between 1980 and 1989")
